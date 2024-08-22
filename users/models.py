0503372d34cccb56128a86a27d57e942369eab86
jimin <jiminseo0307@gmail.com>
2024-08-22 11:53:22 +0900

    Add User model with ChoiceField

--- users/models.py
+++ users/models.py
@@ -4,4 +4,47 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "MALE")
+        FEMALE = ("female", "FEMALE")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean won"
+        USD = "usd", "Dollar"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.ImageField(blank=True)
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+        default="",
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+        null=True,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+        blank=True,
+    )
