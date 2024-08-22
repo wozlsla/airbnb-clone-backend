e3ce2462852fe9db10d0e69ccbf388554f442124
jimin <jiminseo0307@gmail.com>
2024-08-22 15:23:18 +0900

    Add Room Model with Many To Many Field

---
+++ rooms/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class Room(models.Model):
+    """Room Model Definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_romm", "Private Room")
+        SHARED_ROOM = "shared_room", "Shared Room"
+
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+    price = models.PositiveBigIntegerField()
+    rooms = models.PositiveBigIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(
+        max_length=250,
+    )
+    pet_friendly = models.BooleanField(
+        default=True,
+    )
+    kind = models.CharField(
+        max_length=20,
+        choices=RoomKindChoices.choices,
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    amenities = models.ManyToManyField(
+        "rooms.Amenity",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Amenity(models.Model):
+    """Amenity Definition"""
+
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150, null=True)
