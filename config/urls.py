d25da33f174aa06b76f5d18cceb8af3d0a5cd4a2
jimin <jiminseo0307@gmail.com>
2024-08-30 22:19:32 +0900

    django templates URL Arguments, render

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from rooms import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rooms", views.say_hello),
+    path("rooms/", include("rooms.urls")),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
-def say_hello(request):
-    return HttpResponse("Test")
+def see_all_rooms(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "This is rendering test",
+        },
+    )
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"see room with id: {room_id}")
