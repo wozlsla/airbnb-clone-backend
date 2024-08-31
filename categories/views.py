a54219a122901bec7f644e7761766a7ed1e4bf12
jimin <jiminseo0307@gmail.com>
2024-08-31 18:32:09 +0900

    serializer(django core)

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,16 @@
 from django.http import JsonResponse
+from django.core import serializers
 from .models import Category
 
 
 def categories(request):
     all_categories = Category.objects.all()
-    return JsonResponse({"ok": True})
+    return JsonResponse(
+        {
+            "ok": True,
+            "categories": serializers.serialize(
+                "json",
+                all_categories,
+            ),
+        }
+    )
