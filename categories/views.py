9125462e2eec303d55f3d85e3be4fb1f561c5a02
jimin <jiminseo0307@gmail.com>
2024-08-31 20:52:49 +0900

    POST Requests

--- categories/views.py
+++ categories/views.py
@@ -4,8 +4,20 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
+
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
     return Response(serializer.data)
