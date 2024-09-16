326d81031c51b60e1de0c5e6b5bb487d0e222058
jimin <jiminseo0307@gmail.com>
2024-09-16 20:04:08 +0900

    Create Booking validate check_in/out date is future

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,9 +1,35 @@
-from rest_framework.serializers import ModelSerializer
+from django.utils import timezone
+from rest_framework import serializers
 from .models import Booking
 
 
-class PublicBookingSerializer(ModelSerializer):
+class CreateRoomBookingSerializer(serializers.ModelSerializer):
 
+    check_in = serializers.DateField()
+    check_out = serializers.DateField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Date is past. Check again.")
+        return value
+
+    def validate_check_out(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Date is past. Check again.")
+        return value
+
+
+class PublicBookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = (
